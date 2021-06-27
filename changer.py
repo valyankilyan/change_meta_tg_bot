5adbfd50e8de137e432e0f40e78b8c531a525058
valyankilyan <valentin.kil.2002@gmail.com>
2021-06-27 15:48:13 +0300

    fix compress image

--- changer.py
+++ changer.py
@@ -13,38 +13,44 @@ def properCords(cord):
     m = 10 ** ans
     return int(cord * m), m
 
-def getSrcExif(image):
+def makeExif(image, cords):
     # берем размеры
     w, h = image.size
     
-    # грузим готовый exif
-    src_exif = piexif.load(src_photo.info['exif'])
+    zeroth_ifd = {piexif.ImageIFD.Make: "Iphone",  # ASCII, count any
+                piexif.ImageIFD.XResolution: (w, 1),  # RATIONAL, count 1
+                piexif.ImageIFD.YResolution: (h, 1),  # RATIONAL, count 1
+                piexif.ImageIFD.Orientation: 1,
+                piexif.ImageIFD.Software: "ChangeMetaDataBot"  # ASCII, count any
+                }
+    exif_ifd = {piexif.ExifIFD.ExifVersion: b"\x00\x02\x03\x01",  # UNDEFINED, count 4
+                piexif.ExifIFD.LensMake: "Lens",  # ASCII, count any
+                piexif.ExifIFD.Sharpness: 65535,  # SHORT, count 1 ... also be accepted '(65535,)'
+                piexif.ExifIFD.LensSpecification: ((1, 1), (1, 1), (1, 1), (1, 1)),  # Rational, count 4
+                }
     
-    # меняем размеры и ориентацию готового эксифа
-    src_exif["0th"][piexif.ImageIFD.XResolution] = (w, 1)
-    src_exif["0th"][piexif.ImageIFD.YResolution] = (h, 1)
-    src_exif["0th"][piexif.ImageIFD.Orientation] = 1
+    lat_cord, lat_st = properCords(cords.latitude)
+    long_cord, long_st = properCords(cords.longitude)
+    gps_ifd = { piexif.GPSIFD.GPSVersionID: (2, 0, 0, 0),  # BYTE, count 4
+                piexif.GPSIFD.GPSAltitudeRef: 1,  # BYTE, count 1 ... also be accepted '(1,)'
+                piexif.GPSIFD.GPSLatitudeRef: cords.latitude_ref,
+                piexif.GPSIFD.GPSLatitude: [lat_cord, lat_st],
+                piexif.GPSIFD.GPSLongitudeRef: cords.longitude_ref,
+                piexif.GPSIFD.GPSLongitude: [long_cord, long_st]            
+                }
+    exif_dict = {"0th":zeroth_ifd, "Exif":exif_ifd, "GPS":gps_ifd}
+    exif_bytes = piexif.dump(exif_dict)    
 
-    return src_exif
+    return exif_bytes
 
 def changeGPS(path, cords):
     # открываем фотку
     image = Image.open(path)
     
-    # берем для неё готовый эксиф
-    src_exif = getSrcExif(image)
-    
-    # меняем gps кординаты
-    src_exif["GPS"][piexif.GPSIFD.GPSLatitudeRef] = cords.latitude_ref    
-    src_exif["GPS"][piexif.GPSIFD.GPSLatitude] = [properCords(cords.latitude)]
-    src_exif["GPS"][piexif.GPSIFD.GPSLongitudeRef] = cords.longitude_ref    
-    src_exif["GPS"][piexif.GPSIFD.GPSLongitude] = [properCords(cords.longitude)]
-    
-    # дампим эксиф
-    exif_bytes = piexif.dump(src_exif)
+    exif_bytes = makeExif(image, cords)
 
     # заливаем эксиф в фотку
-    image.save(path, "jpeg", exif=exif_bytes)
+    image.save(path, "jpeg", exif=exif_bytes, quality=100)
 
 def deletePhoto(path):
     os.remove(path)
--- testPIL.py
+++ testPIL.py
@@ -37,26 +37,58 @@ for f in files:
     im = Image.open(ph)
     w, h = im.size
     
-    # берем готовый эксиф
-    src_exif = piexif.load(src_photo.info['exif'])
     
-    # меняем у него размеры
-    src_exif["0th"][piexif.ImageIFD.XResolution] = (w, 1)
-    src_exif["0th"][piexif.ImageIFD.YResolution] = (h, 1)
-    src_exif["0th"][piexif.ImageIFD.Orientation] = 1
+
+    zeroth_ifd = {piexif.ImageIFD.Make: "Iphone",  # ASCII, count any
+                piexif.ImageIFD.XResolution: (w, 1),  # RATIONAL, count 1
+                piexif.ImageIFD.YResolution: (h, 1),  # RATIONAL, count 1
+                piexif.ImageIFD.Orientation: 1,
+                piexif.ImageIFD.Software: "piexif"  # ASCII, count any
+                }
+    exif_ifd = {piexif.ExifIFD.ExifVersion: b"\x00\x02\x03\x01",  # UNDEFINED, count 4
+                piexif.ExifIFD.LensMake: "Lens",  # ASCII, count any
+                piexif.ExifIFD.Sharpness: 65535,  # SHORT, count 1 ... also be accepted '(65535,)'
+                piexif.ExifIFD.LensSpecification: ((1, 1), (1, 1), (1, 1), (1, 1)),  # Rational, count 4
+                }
+    lat_cord, lat_st = properCords(latitude)
+    long_cord, long_st = properCords(longitude)
+    gps_ifd = { piexif.GPSIFD.GPSVersionID: (2, 0, 0, 0),  # BYTE, count 4
+                piexif.GPSIFD.GPSAltitudeRef: 1,  # BYTE, count 1 ... also be accepted '(1,)'
+                piexif.GPSIFD.GPSLatitudeRef: latitude_ref,
+                piexif.GPSIFD.GPSLatitude: [lat_cord, lat_st],
+                piexif.GPSIFD.GPSLongitudeRef: longitude_ref,
+                piexif.GPSIFD.GPSLongitude: [long_cord, long_st]            
+                }
+    exif_dict = {"0th":zeroth_ifd, "Exif":exif_ifd, "GPS":gps_ifd}
+    exif_bytes = piexif.dump(exif_dict)
+
+    # round trip
+    # piexif.insert(exif_bytes, "foo.jpg")
+    # exif_dict_tripped = piexif.load("foo.jpg")
+
+
     
-    # меняем жпс
-    src_exif["GPS"][piexif.GPSIFD.GPSLatitudeRef] = latitude_ref
-    cord, st = properCords(latitude)
-    print(cord, st)
-    src_exif["GPS"][piexif.GPSIFD.GPSLatitude] = [cord, st]
-    src_exif["GPS"][piexif.GPSIFD.GPSLongitudeRef] = longitude_ref
-    cord, st = properCords(longitude)
-    print(cord, st)
-    src_exif["GPS"][piexif.GPSIFD.GPSLongitude] = [cord, st]
+    # # берем готовый эксиф
+    # src_exif = piexif.load(src_photo.info['exif'])
+    # # b'0231'
     
-    # дампим эксиф
-    exif_bytes = piexif.dump(src_exif)
+    # # меняем у него размеры
+    # src_exif["0th"][piexif.ImageIFD.XResolution] = (w, 1)
+    # src_exif["0th"][piexif.ImageIFD.YResolution] = (h, 1)
+    # src_exif["0th"][piexif.ImageIFD.Orientation] = 1
+    
+    # # меняем жпс
+    # src_exif["GPS"][piexif.GPSIFD.GPSLatitudeRef] = latitude_ref
+    # cord, st = properCords(latitude)
+    # print(cord, st)
+    # src_exif["GPS"][piexif.GPSIFD.GPSLatitude] = [cord, st]
+    # src_exif["GPS"][piexif.GPSIFD.GPSLongitudeRef] = longitude_ref
+    # cord, st = properCords(longitude)
+    # print(cord, st)
+    # src_exif["GPS"][piexif.GPSIFD.GPSLongitude] = [cord, st]
+    
+    # # дампим эксиф
+    # exif_bytes = piexif.dump(src_exif)
     
     # заливаем эксив в фотку
-    im.save(ph, "jpeg", exif=exif_bytes)
+    im.save('.' + ph.split('.')[1] + '_CHANGED_.' + ph.split('.')[2], "jpeg", exif=exif_bytes)
